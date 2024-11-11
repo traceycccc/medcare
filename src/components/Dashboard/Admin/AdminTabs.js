fe126ec7cd48e30795d45450597c4c608cfc5255
traceycccc <chinghuey07chow@gmail.com>
2024-11-11 15:28:18 +0800

    Initial commit

--- src/components/Dashboard/Admin/AdminTabs.js
+++
@@ -1,8 +0,0 @@
-import DocList from "./DocList/DocList";
-import ViewFeedbacks from "./Feedbacks/ViewFeedbacks";
-import GenerateStats from "./GenerateStats/GenerateStats";
-import StaffList from "./StaffList/StaffList";
-import VerifyUser from "./VerifyUser/VerifyUser";
-import AddNew from "./VerifyUser/AddNew";
-
-export { DocList, ViewFeedbacks, GenerateStats, StaffList, VerifyUser, AddNew };
--- src/components/Dashboard/Doctor/DoctorTabs.js
+++
@@ -1,5 +0,0 @@
-import DocAppointments from "./Appointments/DocAppointments";
-import DocFeedbacks from "./Feedbacks/DocFeedbacks";
-import UploadPrescription from "./UploadPrescription/UploadPrescription";
-
-export { DocAppointments, UploadPrescription, DocFeedbacks };
